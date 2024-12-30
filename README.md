54beff5dc46ec2c0a3a5b2e83f7ec2efca78b8f0
Rich <rs2585@cornell.edu>
2024-12-29 19:53:04 -0500

    Add llm summarization, and part of tts

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # rooporter
 
+## setup
+Setup python venv and install requirements. Activate venv with `source .venv/bin/activate`
+
 1. Fetch news articles
 2. Generate one sentence summary and multi-sentence summary
 3. Generate voice readover from multi-sentence summary
--- rooporter.py
+++ rooporter.py
@@ -3,6 +3,56 @@ from bs4 import BeautifulSoup
 import logging
 import argparse
 import datetime
+import pathlib
+import subprocess
+
+def llm_summarize_articles(articles):
+    repo_path = pathlib.Path(__file__).parent.as_posix()
+    llamaCpp_file_path = repo_path + "/llama.cpp/build/bin/llama-cli"
+    model_file_path = repo_path + "/models/Llama-3.1-8B-Lexi-Uncensored-V2-Q6_K_L.gguf"
+    cpu_threads = "8"
+    output_len = "64"
+    gpu_layers = "12"
+    output_len = "150"
+    context_len = "10000"
+
+    for article in articles:
+        prompt = "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request."
+        prompt += "\n\n### Instruction:\n" + "Please summarize the following article into two sentences. You CANNOT write anything else except the article summary." + "\n\n### Input: " + article['text'] + "\n\n### Response:\n"
+        try:
+            result = subprocess.run(
+                [llamaCpp_file_path,
+                 "-m", model_file_path,
+                 "-t", cpu_threads,
+                 "-n", output_len,
+                 "-ngl", gpu_layers,
+                 "--temp", "0.9",
+                 "-c", context_len,
+                 "-p", prompt],
+                capture_output=True,
+                text=True
+            )
+            llm_output = result.stdout.strip()
+            start_string = "Response:\n"
+            start_idx = llm_output.find(start_string) + len(start_string)
+            extra_ending = " [end of text]"
+            llm_output_stripped = llm_output[start_idx:-len(extra_ending)]
+            article["summarized"] = llm_output_stripped
+        except Exception as e:
+            logging.error(f"Error running subprocess: {e}")
+            continue
+
+def tts(article_data):
+    from melo.api import TTS
+
+    # TODO make this a for loop to generate speech for summarized version
+    speed = 1.2
+    device = 'auto' # Will automatically use GPU if available
+    model = TTS(language='EN', device=device)
+    speaker_ids = model.hps.data.spk2id
+    output_path = 'en-us.wav'
+    # TODO replace first arg
+    model.tts_to_file(article_data['headline'], speaker_ids['EN-US'], output_path, speed=speed)
 
 def parse_article(article_url):
     # Fetch the article
@@ -87,7 +137,7 @@ if __name__ == "__main__":
 
     # Set up logging
     log_level = getattr(logging, args.log.upper(), logging.INFO)
-	year_month_date = datetime.datetime.now().strftime("%Y_%m_%d")
+    year_month_date = datetime.datetime.now().strftime("%Y_%m_%d")
     logging.basicConfig(
         level=log_level,
 		filename = 'rooporter' + year_month_date + '.log',
@@ -95,13 +145,10 @@ if __name__ == "__main__":
 		datefmt='%H:%M:%S'
     )
 
-    # Scrape CNN
     base_url = "https://www.cnn.com"
     articles = scrape_homepage(base_url, limit=args.limit)
 
-    # Display articles
-    for article in articles:
-        print(f"Headline: {article['headline']}\n")
-        print(f"Text: {article['text'][:500]}...\n")  # Print the first 500 characters
-        print(f"URL: {article['url']}\n")
-        print("=" * 80)
+    llm_summarize_articles(articles)
+
+    # TODO run TTS on summarized article text
+    #tts(article_data)
