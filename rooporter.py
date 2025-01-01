9625392f26125358512f317cc3e571a287ae4d68
Rich <rs2585@cornell.edu>
2024-12-31 21:13:35 -0500

    testing video generation

--- rooporter.py
+++ rooporter.py
@@ -6,6 +6,62 @@ import datetime
 from pathlib import Path
 import subprocess
 
+from HunyuanVideo.hyvideo.utils.file_utils import save_videos_grid
+from HunyuanVideo.hyvideo.config import parse_args
+from HunyuanVideo.hyvideo.inference import HunyuanVideoSampler
+
+def generate_videos(prompts):
+    args = parse_args()
+    #logger.info(f'Making video with prompt: {prompt}')
+    args.prompt = "a cat dancing"
+    args.video_size[0] = 960
+    args.video_size[1] = 544
+    args.video_length = 129
+    args.seed = 0
+    args.infer_steps = 30
+    #args.use_cpu_offload
+
+    models_root_path = Path(args.model_base)
+    if not models_root_path.exists():
+        raise ValueError(f"`models_root` not exists: {models_root_path}")
+    
+    # Create save folder to save the samples
+    save_path = args.save_path if args.save_path_suffix=="" else f'{args.save_path}_{args.save_path_suffix}'
+    if not os.path.exists(args.save_path):
+        os.makedirs(save_path, exist_ok=True)
+
+    # Load models
+    hunyuan_video_sampler = HunyuanVideoSampler.from_pretrained(models_root_path, args=args)
+    
+    # Get the updated args
+    args = hunyuan_video_sampler.args
+
+    # Start sampling
+    outputs = hunyuan_video_sampler.predict(
+        prompt=args.prompt, 
+        height=args.video_size[0],
+        width=args.video_size[1],
+        video_length=args.video_length,
+        seed=args.seed,
+        negative_prompt=args.neg_prompt,
+        infer_steps=args.infer_steps,
+        guidance_scale=args.cfg_scale,
+        num_videos_per_prompt=args.num_videos,
+        flow_shift=args.flow_shift,
+        batch_size=args.batch_size,
+        embedded_guidance_scale=args.embedded_cfg_scale
+    )
+    samples = outputs['samples']
+    
+    # Save samples
+    if 'LOCAL_RANK' not in os.environ or int(os.environ['LOCAL_RANK']) == 0:
+        for i, sample in enumerate(samples):
+            sample = samples[i].unsqueeze(0)
+            time_flag = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d-%H:%M:%S")
+            save_path = f"{save_path}/{time_flag}_seed{outputs['seeds'][i]}_{outputs['prompts'][i][:100].replace('/','')}.mp4"
+            save_videos_grid(sample, save_path, fps=24)
+            logger.info(f'Sample save to: {save_path}')
+
 def process_videos_and_audio(audio_video_mapping, final_output_file):
     """
     Combines video files associated with audio files, adds the audio, 
@@ -227,6 +283,8 @@ if __name__ == "__main__":
     parser.add_argument('--limit', type=int, default=5, help="Number of articles to scrape")
     parser.add_argument('--log', type=str, default='info', help="Log level (debug, info, warning, error, critical)")
     args = parser.parse_args()
+    generate_videos([])
+    exit()
 
     # Set up logging
     log_level = getattr(logging, args.log.upper(), logging.INFO)
