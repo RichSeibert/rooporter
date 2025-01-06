5de7ced67e848b64a58949604bfb5a7b53880c00
Rich <rs2585@cornell.edu>
2025-01-05 20:50:30 -0500

    fix missing quote

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ audio_dir = "tmp/audio/"
 video_dir = "tmp/video/"
 setup_dirs([audio_dir, video_dir])
 import nltk
-nltk.download('averaged_perceptron_tagger_eng', download_dir=".venv)
+nltk.download('averaged_perceptron_tagger_eng', download_dir=".venv")
 
 
 setup(
