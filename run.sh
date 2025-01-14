5d6faa1c70a667aae756296548c4709da1db5537
Rich <rs2585@cornell.edu>
2025-01-13 23:52:39 -0500

    Move runpod scripts to another repo which will be put on the controller machine

--- run.sh
+++ run.sh
@@ -6,10 +6,3 @@ bash setup.sh
 echo "Run rooporter.py"
 source .venv/bin/activate
 python rooporter.py
-
-# TODO get this to work. config fails while running inside of pod. Made terminate_runpod_instance.sh for now. That script can be deleted once this section is fixed
-echo "Deleting pod"
-api_key=$(<runpod_api_key)
-runpodctl config --apiKey $api_key
-pod_id=$(runpodctl get pod | awk 'NR==2 {print $1}')
-runpodctl remove pod $pod_id
--- terminate_runpod_instance.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Deleting pod"
-api_key=$(</home/rich/Documents/rooporter/runpod_api_key)
-runpodctl config --apiKey $api_key
-pod_id=$(runpodctl get pod | awk 'NR==2 {print $1}')
-runpodctl remove pod $pod_id
