28a083834dc232b9b04784549fd926342c49f869
Rich <rs2585@cornell.edu>
2025-03-22 23:29:23 -0400

    setup

--- setup.sh
+++ setup.sh
@@ -99,6 +99,12 @@ if [ ! -L "models" ]; then
     fi
     huggingface-cli download bartowski/Llama-3.1-8B-Lexi-Uncensored-V2-GGUF Llama-3.1-8B-Lexi-Uncensored-V2-Q8_0.gguf --local-dir ../models
     ln -s ../models models
+    huggingface-cli download Wan-AI/Wan2.1-T2V-14B --local-dir ../models/Wan2.1-T2V-14B
+    ln -s ../Wan2.1-T2V-14B Wan2.1-T2V-14B
+    huggingface-cli download stabilityai/stable-audio-open-1.0 --local-dir ../models/stable-audio-open1.0
+    ln -s ../stable-audio-open1.0 stable-audio-open1.0
+    huggingface-cli download hexgrad/Kokoro-82M --local-dir ../models/Kokoro-82M
+    ln -s ../models/Kokoro-82M Kokoro-82M
 fi
 
 if [ "$MODE" == "0" ] || [ "$MODE" == "1" ]; then
@@ -108,18 +114,6 @@ if [ "$MODE" == "0" ] || [ "$MODE" == "1" ]; then
         cd Wan2.1
         pip install -r requirements.txt
         cd ..
-    fi
-    if [ ! -L "Wan2.1-T2V-14B" ]; then
-        huggingface-cli download Wan-AI/Wan2.1-T2V-14B --local-dir ../models/Wan2.1-T2V-14B
-        ln -s ../Wan2.1-T2V-14B Wan2.1-T2V-14B
-    fi
-    if [ ! -L "stable-audio-open1.0" ]; then
-        huggingface-cli download stabilityai/stable-audio-open-1.0 --local-dir ../models/stable-audio-open1.0
-        ln -s ../stable-audio-open1.0 stable-audio-open1.0
-    fi
-    if [ ! -L "Kokoro-82M" ]; then
-        huggingface-cli download hexgrad/Kokoro-82M --local-dir ../models/Kokoro-82M
-        ln -s ../models/Kokoro-82M Kokoro-82M
     fi
 elif [ "$MODE" == "2" ]; then
     echo "Setting up Hunyuan and MeloTTS for mode 2"
