0a93c417bd829871f70e489369d631772a579e2f
Rich <rs2585@cornell.edu>
2025-03-23 17:28:13 -0400

    add comment

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO figure out a way to download apt stuff to a non-volitile directory, so it's
+# not deleted when starting a new instance. I shouldn't need to run setup.sh 
+# every time
 echo "Run setup.sh"
 bash setup.sh
 
