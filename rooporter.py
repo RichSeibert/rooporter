72548f5197409246c8d913a9054a49519613a361
Rich <rs2585@cornell.edu>
2025-03-23 17:36:04 -0400

    typo

--- rooporter.py
+++ rooporter.py
@@ -452,7 +452,7 @@ def create_topic_based_videos(config_settings, hf_token):
     try:
         # TODO fix filename, right now it's hardcoded to 0_X.wav
         # diffsynth_wan_multithread(prompts_today["videos"], num_frames, fps)
-        input_data = [[i, prompt, num_frames, fps] for i, prompt in enumerate(prompts)]
+        input_data = [[i, prompt, num_frames, fps] for i, prompt in enumerate(prompts_today["videos"])]
         for inpu in input_data:
             diffsynth_wan(inpu)
     except Exception as e:
